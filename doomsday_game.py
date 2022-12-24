952dd3de2c41d98ad587993e9eaa6ce3c002af4c
CHaerem <chris.haerem@gmail.com>
2022-12-24 15:52:46 +0100

    Added landing page

--- doomsday_game.py
+++ doomsday_game.py
@@ -3,7 +3,15 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/run_quiz', methods=['GET'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/learning')
+def learning():
+    return render_template('learning.html')
+
+@app.route('/quiz')
 def run_quiz():
     return render_template('quiz.html')
 
