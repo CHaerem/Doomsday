b85714531934286af3c5575106944f4186276ef6
CHaerem <chris.haerem@gmail.com>
2022-12-24 13:10:12 +0100

    Update doomsday_game.py

--- doomsday_game.py
+++ doomsday_game.py
@@ -7,5 +7,6 @@ def run_quiz():
     return render_template('quiz.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
 
